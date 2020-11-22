2c70bfd3b127e364ed95f1d76238117dd5e47352
Bastien MAURICE <bastien_maurice@hotmail.com>
2020-11-22 23:52:09 +0100

    .

--- App.tsx
+++ App.tsx
@@ -1,19 +1,53 @@
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import MainTabNavigator from './src/navigation/tabNavigator';
 
-export default function App() {
-  return (
-    <MainTabNavigator/>
-  );
+
+let customFonts = {
+    'Arial': require('./src/assets/fonts/arial.ttf'),
+}
+interface iState {
+    fontsLoaded: boolean;
+}
+interface IProps {
+}
+export default class App extends React.Component<IProps, iState> {
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            fontsLoaded: false
+        }
+    }
+
+    async loadFontsAsync() {
+        await Font.loadAsync(customFonts);
+        this.setState({ fontsLoaded: true });
+    }
+
+    componentDidMount() {
+        this.loadFontsAsync();
+    }
+
+    render() {
+        if (!this.state.fontsLoaded) {
+            return <AppLoading />;
+        } else {
+            return (
+                <SafeAreaView style={styles.container}>
+                    <MainTabNavigator />
+                </SafeAreaView>
+            );
+        }
+
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
 });
